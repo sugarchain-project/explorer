87c0376c0b17c4941f8e5fdc119f6b89cceb4681
cryptozeny <cryptozeny@gmail.com>
2019-02-02 00:58:57 +0900

    Sugarchain: fix: sh

--- run.sh
+++ run.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-#start explorer
-forever start bin/cluster
+# coind daemon
+$HOME/sugarchain-v0.16.3/src/sugarchaind -server=1 -rpcuser=username -rpcpassword=password -txindex -daemon -testnet
 
-#update
+# waiting for daemon
+echo "***** sleep 30 seconds for daemon *****"
+sleep 30.0
+
+# start explorer
+cd $HOME/explorer/
+touch ./tmp/index.pid
+rm -f ./tmp/index.pid
+$HOME/.nvm/v0.10.28/bin/forever start ./bin/cluster
+
+# waiting for explorer
+echo "***** sleep 30 seconds for explorer *****"
+sleep 30.0
+
+# update
 while true;
-do 
-    touch tmp/index.pid
-    rm -f ./tmp/index.pid 
-    node scripts/sync.js index update
+do
+    cd $HOME/explorer/
+    touch ./tmp/index.pid
+    rm -f ./tmp/index.pid
+    node ./scripts/sync.js index update
     # node scripts/sync.js market
-    node scripts/peers.js
+    node ./scripts/peers.js
     echo "***** sleep 5 seconds *****"
     sleep 5.0
 done
